b3e78eb83710057a018718daf87b7976c7134cf5
akostom <akostom@gulfgasandpower.nl>
2024-12-31 01:31:51 +0100

    init order

---
+++ src/main/java/com/webshop/order/model/Order.java
@@ -0,0 +1,29 @@
+package com.webshop.order.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate = LocalDateTime.now();
+}
---
+++ src/main/java/com/webshop/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.webshop.order.repository;
+
+import com.webshop.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
